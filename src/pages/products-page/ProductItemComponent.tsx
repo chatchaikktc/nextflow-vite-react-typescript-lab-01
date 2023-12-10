cacabf26464f28a864d87fa243a189e75f110db1
Teerasej <teerasej@gmaill.com>
2023-12-10 16:29:00 +0700

    setup a product item layout

---
+++ src/pages/products-page/ProductItemComponent.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const ProductItemComponent: React.FC = () => {
+    return (
+        <div className="bg-white rounded-lg shadow-md p-4 border border-gray-300">
+            <h2 className="text-xl font-bold mb-4">Product Title</h2>
+            <img src="product-image.jpg" alt="Product Image" className="w-full mb-4" />
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                More Details
+            </button>
+        </div>
+    );
+};
+
+export default ProductItemComponent;
