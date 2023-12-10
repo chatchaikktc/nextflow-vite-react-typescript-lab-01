fd5cf7ab31868baaee01ed1907cab10e634f41e5
Teerasej <teerasej@gmaill.com>
2023-12-10 15:57:59 +0700

    make hover color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 export default function NavBar() {
   return (
     <nav className="bg-black text-white">
-      <div className="flex justify-between p-5">
-        <a href="/" className="text-2xl font-bold">Home</a>
-        <a href="/products" className="text-2xl font-bold">Products</a>
+      <div className="flex">
+        <a href="/" className="text-1m font-bold p-3 hover:bg-red-600">Home</a>
+        <a href="/products" className="text-1m font-bold p-3  hover:bg-red-600">Products</a>
       </div>
     </nav>
   )
