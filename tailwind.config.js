fd02445b2e610c6a38687f9740809cbc5648026f
Teerasej <teerasej@gmaill.com>
2023-12-10 16:28:44 +0700

    set predefine width value to use as tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1024': '1024px',
+      },
+    },
   },
   plugins: [],
 }
