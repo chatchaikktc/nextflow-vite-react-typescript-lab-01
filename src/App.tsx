0e8683dacd61995b3e30948c63d1e13deaba96bb
Teerasej <teerasej@gmaill.com>
2023-12-10 15:54:56 +0700

    import component and define a menu and display with component from react-router-dom'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/home-page/HomePage';
+import ProductsPage from './pages/products-page/ProductsPage';
 function App() {
-  
+
 
   return (
     <>
-      <h1 className="text-6xl font-bold underline">
-        Hello world!
-      </h1>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Routes>
+      </Router>
     </>
   )
 }
