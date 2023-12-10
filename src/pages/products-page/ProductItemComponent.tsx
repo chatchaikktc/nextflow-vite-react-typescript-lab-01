4041396f5ff4d1be84230dde26e497e989af032e
Teerasej <teerasej@gmaill.com>
2023-12-10 17:33:10 +0700

    pass product object via props to show in item's layout

--- src/pages/products-page/ProductItemComponent.tsx
+++ src/pages/products-page/ProductItemComponent.tsx
@@ -1,13 +1,20 @@
 
 import React from 'react';
 import imageUrl from './../../assets/toy-box.webp';
+import IProduct from './IProduct';
 
-const ProductItemComponent: React.FC = () => {
+interface IProductItemComponentProps {
+    key: string;
+    product: IProduct;
+}
+
+const ProductItemComponent = ({ product }: IProductItemComponentProps) => {
+
+    
     return (
         <div className="bg-white rounded-lg shadow-md p-4 border border-gray-300">
-            <h2 className="text-xl font-bold mb-4">Product Title</h2>
+            <h2 className="text-xl font-bold mb-4">{product.title}</h2>
             <img src={imageUrl} alt="Product Image" className="w-fit mb-4 " />
-            
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                 More Details
             </button>
