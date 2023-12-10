c48f06ee4bf91eaf5fa763f47a586f2c5cb74370
Teerasej <teerasej@gmaill.com>
2023-12-10 17:32:34 +0700

    import json file as product array

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -1,22 +1,24 @@
 
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import ProductItemComponent from './ProductItemComponent';
+import IProduct from './IProduct';
+import ProductJSON from "./../../data/products.json";
+
 
 export default function ProductsPage() {
+
+  const products: IProduct[] = ProductJSON.products;
+
   return (
     <div className="flex justify-center">
       <div className="pt-10 m-10 bg-slate-200 w-1024">
-
         <h1 className="text-3xl mb-5 text-center">Products Page</h1>
-
         <div className="grid grid-cols-2 gap-4">
-          <ProductItemComponent />
-          <ProductItemComponent />
-          <ProductItemComponent />
-          <ProductItemComponent />
-          <ProductItemComponent />
+          {products.map((product) => (
+            <ProductItemComponent key={product.title} product={product} />
+          ))}
         </div>
-
       </div>
     </div>
   );
