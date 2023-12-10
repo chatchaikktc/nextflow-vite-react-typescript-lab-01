edcb7d0a4b456a03adcd48810ef971d761aba43c
Teerasej <teerasej@gmaill.com>
2023-12-10 16:45:29 +0700

    Show image with div element to crop the image

--- src/pages/products-page/ProductItemComponent.tsx
+++ src/pages/products-page/ProductItemComponent.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
+import imageUrl from './../../assets/toy-box.webp';
 
 const ProductItemComponent: React.FC = () => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4 border border-gray-300">
             <h2 className="text-xl font-bold mb-4">Product Title</h2>
-            <img src="product-image.jpg" alt="Product Image" className="w-full mb-4" />
+            <img src={imageUrl} alt="Product Image" className="w-fit mb-4 " />
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                 More Details
             </button>
