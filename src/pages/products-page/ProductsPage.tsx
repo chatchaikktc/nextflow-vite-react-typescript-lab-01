d28dacdc2ec1aa2c32e0844dfc8d147bc39e896f
Teerasej <teerasej@gmaill.com>
2023-12-10 16:29:07 +0700

    show it in products page

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+
+import React from 'react';
+import ProductItemComponent from './ProductItemComponent';
 
 export default function ProductsPage() {
   return (
-    <div>Products Page</div>
-  )
+    <div className="flex justify-center">
+      <div className="pt-10 m-10 bg-slate-200 w-1024">
+
+        <h1 className="text-3xl mb-5 text-center">Products Page</h1>
+
+        <div className="grid grid-cols-2 gap-4">
+          <ProductItemComponent />
+          <ProductItemComponent />
+          <ProductItemComponent />
+          <ProductItemComponent />
+          <ProductItemComponent />
+        </div>
+
+      </div>
+    </div>
+  );
 }
