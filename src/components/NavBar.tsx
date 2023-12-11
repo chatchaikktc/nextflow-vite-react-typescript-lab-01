fa2a90beb859a976bc12b157a06a31acb74737b0
Teerasej <teerasej@gmaill.com>
2023-12-11 19:10:53 +0700

    สลับไปใช้ Link ของ react-router-dom แทน <a> เพื่อหลีกเลี่ยงการโหลดหน้าใหม่ และทำให้ตัวแปรทั้งหมดรวมถึง useContext ถูกรีเซ็ต

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 
 import React from 'react'
 import CartCounter from './CartCounter'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
     <nav className="bg-black text-white">
       <div className="flex justify-between">
         <div className='flex'>
-          <a href="/" className="text-1m font-bold p-3 hover:bg-red-600">Home</a>
-          <a href="/products" className="text-1m font-bold p-3  hover:bg-red-600">Products</a>
+          <Link to="/" className="text-1m font-bold p-3 hover:bg-red-600">Home</Link>
+          <Link to="/products" className="text-1m font-bold p-3  hover:bg-red-600">Products</Link>
         </div>
         <CartCounter/>
       </div>
