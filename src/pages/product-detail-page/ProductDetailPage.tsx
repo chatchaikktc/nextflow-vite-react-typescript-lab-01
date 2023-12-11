ec96a767c83e14c7d39bea33d72d8a169831b3a2
Teerasej <teerasej@gmaill.com>
2023-12-11 17:47:22 +0700

    Removed

--- src/pages/product-detail-page/ProductDetailPage.tsx
+++ src/pages/product-detail-page/ProductDetailPage.tsx
@@ -1,17 +1,22 @@
+import { useParams } from "react-router-dom";
+import { useProducts } from "../../contexts/ProductProvider";
+import IProduct from "../products-page/IProduct";
 
 
 
 export default function ProductDetailPage() {
+    const { id } = useParams<{ id: string }>();
+    const products:IProduct[] = useProducts();
+    const product = products.find( product => product.id === Number(id));
 
     return (
         <div className="flex justify-center">
             <div className="pt-10 m-10 bg-slate-200 w-1024">
                 <div className="bg-white rounded-lg shadow-md p-4 border border-gray-300">
-                    <h2 className="text-xl font-bold mb-4">Product Title</h2>
-                    <img src="" alt="Product Image" className="w-fit mb-4 " />
+                    <h2 className="text-xl font-bold mb-4">{product?.title}</h2>
                     <div className="flex flex-row justify-between">
                         <p className="p-2">
-                            ราคา Price บาท
+                            ราคา {product?.price} บาท
                         </p>
                         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded float-right">
                             Add to Cart
