1f86ffa931b77c2d9727f840dcdbe546afe88a38
Teerasej <teerasej@gmaill.com>
2023-12-11 18:05:58 +0700

    กำหนดตัวแปร และ function ใน provider 1. สร้างตัวแปร state เก็บข้อมูลสินค้าในตะกร้า อันนี้เป็น array 2. สร้าง function ที่จะรับ product (กดจากปุ่ม add to cart) และอัพเดตในตัวแปร state 3. ใส่ตัวแปร และ function ที่สร้าง ไว้ให้กับ context จะได้เรียกใช้จากที่ไหนก็ได้ 4. สร้าง hook ชื่อ useCart เพื่อที่จะเรียกใช้จาก component ไหนก็ได้

--- src/contexts/CartProvider.tsx
+++ src/contexts/CartProvider.tsx
@@ -1,13 +1,32 @@
-import { ReactNode } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
+import IProduct from "../pages/products-page/IProduct";
 
 interface ICartProviderProps {
     children: ReactNode
 }
 
+interface ICartContext {
+    itemsInCart: IProduct[];
+    addToCart: (product: IProduct) => void;
+}
+
+const CartContext = createContext<ICartContext>({ itemsInCart: [], addToBasket: () => {} });
+
+
 export default function CartProvider({ children }: ICartProviderProps) {
+
+    const [itemsInCart, setItemsInCart] = useState<IProduct[]>([]);
+
+    const addToCart = (product:IProduct) => {
+        itemsInCart.push(product)
+        setItemsInCart(itemsInCart);
+    }
+
   return (
-    <div>
+    <CartContext.Provider value={{ itemsInCart, addToCart }}>
         {children}
-    </div>
+    </CartContext.Provider>
   )
 }
+
+export const useCart = () => useContext(CartContext);
