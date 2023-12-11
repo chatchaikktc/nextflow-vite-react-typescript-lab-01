71a1ef5a56406aad1a0c3f76733b38fb76de7fef
Teerasej <teerasej@gmaill.com>
2023-12-11 17:44:21 +0700

    ใช้ Image จาก public folder url

--- src/pages/products-page/ProductItemComponent.tsx
+++ src/pages/products-page/ProductItemComponent.tsx
@@ -17,7 +17,7 @@ const ProductItemComponent = ({ product }: IProductItemComponentProps) => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4 border border-gray-300">
             <h2 className="text-xl font-bold mb-4">{product.title}</h2>
-            <img src={`src/${product?.image}`} alt="Product Image" className="w-fit mb-4 " />
+            <img src={product.image} alt="Product Image" className="w-fit mb-4" />
             <button
                 className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
                 onClick={handleMoreDetails}
