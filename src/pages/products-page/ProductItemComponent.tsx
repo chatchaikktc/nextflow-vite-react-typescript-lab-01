00d63bcf1f22d15b68428ff69a0482845d07ebb2
Teerasej <teerasej@gmaill.com>
2023-12-11 19:59:25 +0700

    add comment as a guideline

--- src/pages/products-page/ProductItemComponent.tsx
+++ src/pages/products-page/ProductItemComponent.tsx
@@ -11,8 +11,9 @@ const ProductItemComponent = ({ product }: IProductItemComponentProps) => {
 
     return (
         <div className="bg-white rounded-lg shadow-md p-4 border border-gray-300">
-            <h2 className="text-xl font-bold mb-4">{product.title}</h2>
-            <img src={product.image} alt="Product Image" className="w-fit mb-4" />
+            <h2 className="text-xl font-bold mb-4">Product Title</h2>
+            {/* product.image */}
+            <img src="" alt="Product Image" className="w-fit mb-4" />
             <button
                 className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
             >
