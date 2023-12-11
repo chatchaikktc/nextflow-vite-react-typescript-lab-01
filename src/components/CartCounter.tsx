2cb5420659ed21798a2d5d46fda1f85dfaf121ef
Teerasej <teerasej@gmaill.com>
2023-12-11 17:55:24 +0700

    สร้าง cart counter component

---
+++ src/components/CartCounter.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export default function CartCounter() {
+  return (
+    <div className="text-1m font-bold p-3 hover:bg-red-600">Cart: 0</div>
+  )
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 
 import React from 'react'
+import CartCounter from './CartCounter'
 
 export default function NavBar() {
   return (
     <nav className="bg-black text-white">
-      <div className="flex">
-        <a href="/" className="text-1m font-bold p-3 hover:bg-red-600">Home</a>
-        <a href="/products" className="text-1m font-bold p-3  hover:bg-red-600">Products</a>
+      <div className="flex justify-between">
+        <div className='flex'>
+          <a href="/" className="text-1m font-bold p-3 hover:bg-red-600">Home</a>
+          <a href="/products" className="text-1m font-bold p-3  hover:bg-red-600">Products</a>
+        </div>
+        <CartCounter/>
       </div>
     </nav>
   )
