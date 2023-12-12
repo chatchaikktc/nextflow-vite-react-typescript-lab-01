ccb9c3a3d89d419f2ae3921f26f8659b4077f824
Chatchai.k <chatchai.k@ktc.co.th>
2023-12-12 17:53:21 +0700

    learning typescript-2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import path, { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASE_PUBLIC_URL,
+    build: {
+      cssCodeSplit: false,
+      rollupOptions: {
+        treeshake: true,
+        input: {
+          main: resolve(__dirname, "index.html"),
+        },
+        output: {
+          assetFileNames: (assetInfo) => {
+            const fileExt = assetInfo.name.split(".").pop();
+
+            if (fileExt === "css") {
+              return `assets/[name]-[hash].[ext]`;
+            }
+
+            return `assets/[name].[ext]`;
+          },
+        },
+      },
+      chunkSizeWarningLimit: 1500,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [react()],
+  };
+});
